9b789e8535328b5bf0d27ea4a568e647a906d476
Icaro Oliveira <icarovinici@gmail.com>
2020-12-25 18:20:32 -0300

    feat: add header menu component

---
+++ src/components/Layout/HeaderMenu/index.tsx
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react'
+import { ThemeContext } from 'styled-components'
+import { DarkModeSwitch } from 'react-toggle-dark-mode'
+import { Link } from 'gatsby'
+
+import { useToggleTheme } from '~app/hooks/toggleTheme'
+
+import * as Styled from './styles'
+
+const HeaderMenu: React.FC = () => {
+  const { title } = useContext(ThemeContext)
+  const { toggleTheme } = useToggleTheme()
+
+  return (
+    <Styled.Container>
+      <Link to="/">Blog.</Link>
+
+      <DarkModeSwitch checked={title === 'dark'} onChange={toggleTheme} />
+    </Styled.Container>
+  )
+}
+
+export default HeaderMenu
---
+++ src/components/Layout/HeaderMenu/styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const Container = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+  max-width: 1020px;
+  margin-left: auto;
+  margin-right: auto;
+
+  padding: 2rem 3rem 2rem;
+
+  > a {
+    font-size: 3rem;
+    font-weight: 700;
+    line-height: 1;
+    text-decoration: none;
+
+    color: ${props => props.theme.colors.primary};
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`
