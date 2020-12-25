a65c3d85ed34eccf74acb7de33b6c5896e90de05
Icaro Oliveira <icarovinici@gmail.com>
2020-12-25 18:22:43 -0300

    feat: add icons paths

---
+++ src/utils/getPaths.ts
@@ -0,0 +1,18 @@
+/* eslint-disable global-require */
+export const getPath = {
+  icons: {
+    email: require('../../static/assets/icons/email.svg'),
+    facebook: require('../../static/assets/icons/facebook.svg'),
+    github: require('../../static/assets/icons/github.svg'),
+  },
+  urls: {
+    externalLinks: {
+      email: 'mailto:icarovinici@gmail.com',
+      facebook: 'https://www.facebook.com/icaro.vcarvalho',
+      linkedin: 'https://www.linkedin.com/in/icaroov/',
+      instagram: 'https://www.instagram.com/icaroov',
+      github: 'https://github.com/icaroov',
+      gatsby: 'https://www.gatsbyjs.com/',
+    },
+  },
+}
